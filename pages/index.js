44fdd7a88cad3e6ae8718d86a61503466efe4fa3
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-04-23 04:03:02 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
-import LainOne from './componet/lainone';
-import LainTow from './componet/laintow';
-import LainTree from './componet/laintree';
+
 
 export default function Home() {
 
@@ -47,33 +45,7 @@ const handelnextmovie=()=>{
 
      
 
-        {/* <div className="background" style={{ backgroundImage: `url(/movie${img}.jpg) `}}>       
-          <div className='leftright'> <img src="rightt.svg" alt="" width={30} className='right' onClick={handelnextmovie}/><img src="rightt.svg" alt="" width={30} className='left' onClick={handelprevmovie} />  
-            </div> 
-               <section dir='rtl' className='shahd'>
-                <h4 onClick={()=>router.push("./film/homemovie")}>شاهد الان</h4>
-                <h4>اضف الى القائمة</h4>
-                <h4>مشاهدة العرض التشويقي</h4>
-              </section>
-       </div>
-      <div className='moviest'>
-            <div className='mov' dir='rtl'>
-            <section onClick={()=>setimg(1)} style={{ backgroundImage: `url(/movie1.jpg)`}}></section>
-            <section onClick={()=>setimg(2)} style={{ backgroundImage: `url(/movie2.jpg)`}}></section>
-            <section onClick={()=>setimg(3)} style={{ backgroundImage: `url(/movie3.jpg)`}}></section>
-            <section onClick={()=>setimg(4)} style={{ backgroundImage: `url(/movie4.jpg)`}}></section>
-            <section onClick={()=>setimg(5)} style={{ backgroundImage: `url(/movie5.jpg)`}}></section>
-            <section onClick={()=>setimg(6)} style={{ backgroundImage: `url(/movie6.jpg)`}}></section>
-            
-        </div>
-      </div>
-
-
-        <LainOne />
-        <LainTow/>
-        <LainTree/>
-        <LainOne />
-        <LainTow/> */}
+    
         
 
     </div>
