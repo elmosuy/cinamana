010cf3a67c4ef9b4797959cff47b64668f46b263
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-04-24 01:07:20 +0300

    Add files via upload

---
+++ pages/componet/laintree.js
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { stringify } from 'querystring';
+import React ,{useState,useEffect}from 'react'
+import data from "../api/api_nice.json"
+
+const LainOne = () => {
+   
+    
+        const router = useRouter();
+        const [val, setval] = useState()
+        useEffect(() => {
+            const id = document.getElementById("val")
+            setval(id.value)
+            
+        }, [])
+        
+
+  return (
+    <div className='alllain'>
+       
+        <h4>الاكثر مشاهدة  {val}</h4>
+        <div className='lainone' dir='rtl' id='linone'>
+           {
+            data.map((el)=>(
+              <section key={el.id} onClick={()=>router.push(`/componet/navbar-list/${el.id}`)} className="item" >
+              <div className='im'><Image  width={500}  height={350} src={el.Image}  />
+                  <p>{el.title}</p>
+                  <p id='val'>رومانسي 2021</p>
+              </div>
+             </section>
+            ))
+           }
+           
+          
+        </div>
+    </div>
+  )
+}
+
+export default LainOne
