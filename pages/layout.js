0e0dff6e3e26c6c464aad1aee2f47b7777f5e78b
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-05-01 16:53:12 +0300

    Add files via upload

--- pages/layout.js
+++ pages/layout.js
@@ -85,24 +85,28 @@ useEffect(() => {
             height={35}
           />
         </div>
-        <div className="searching">
+        <div className="searching" >
+          <span>
           <input
+           
             type="text"
             name="search"
-            className="search"
+            className="search search2"
             onClick={handelsearch}
             placeholder="ابحث عن فلم او مسلسل"
             onChange={(e) => {
               setsearch(e.target.value.toLowerCase());
             }}
-          />
-      
-
-        <svg viewBox="0 0 512 512" width={25} className="ionicon" xmlns="http://www.w3.org/2000/svg">
+          /> 
+          
+           <svg viewBox="0 0 512 512" width={25} className="ionicon" xmlns="http://www.w3.org/2000/svg">
             <title>Search</title>
             <path stroke-width="32" stroke-miterlimit="10" stroke="currentColor" fill="none" d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"></path>
             <path d="M338.29 338.29L448 448" stroke-width="32" stroke-miterlimit="10" stroke-linecap="round" stroke="currentColor" fill="none"></path>
         </svg>
+      </span>
+
+      
   
 
           <div className="searcher" id="searcher">
