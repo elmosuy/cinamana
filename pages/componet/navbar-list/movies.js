f0ac51e6c1806c11203c1420e19f06829843d364
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-05-02 18:14:22 +0300

    Add files via upload

--- pages/componet/navbar-list/movies.js
+++ pages/componet/navbar-list/movies.js
@@ -2,8 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image'
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
-import data from "../../api/api_hollyood2.json"
-import datas from "../../api/api_hollywood.json"
+import datas from "../../api/search_en.json"
 import Link from 'next/link';
 
 const Movies = () => {
@@ -13,21 +12,43 @@ const Movies = () => {
     const rangeType = document.getElementById("rangeType");
     rangeType.classList.toggle("show-range");
   };
-const [post, setpost] = useState([])
- const scroll=()=>{
-    setTimeout(()=>{
-      setpost(datas)
-     }, 6000)}
-  useEffect(() => {
+// const [post, setpost] = useState([])
+//  const scroll=()=>{
+//     setTimeout(()=>{
+//       setpost(datas)
+//      }, 6000)}
+//   useEffect(() => {
 
-      scroll()
+//       scroll()
 
-  })
+//   })
   
 
-scroll()
+// scroll()
+const [select, setselect] = useState("مترجم")
 
- 
+const filteredDaa = datas.filter((el) => {return el.type.includes(select)})
+   
+  console.log(select);
+useEffect(() => {
+  if(filteredDaa.length==80){
+  document.getElementById("more-move").style.display="flex"
+}else if(filteredDaa.length>=160){
+  document.getElementById("more-move").style.display="flex"
+
+}else {
+  document.getElementById("more-move").style.display="none"
+
+}
+})
+
+
+
+const [n, setn] = useState(70)
+const loop=()=>{
+setn(n+70)
+
+}
 
 
   return (
@@ -43,24 +64,21 @@ scroll()
           <ul>
             <li >افلام</li>
 <Link href="/componet/series"><li>مسلسلات</li></Link>
-             <select name="انواع" id="">
-            <option value="">الكل</option>
-              <option value="">كوميدي</option>
-              <option value="">دراما</option>
-              <option value="">رومانسي</option>
-              <option value="">مثير</option>
-              <option value="">افلام قصيرة</option>
-              <option value="">اكشن</option>
-              <option value="">برامج</option>
-              <option value="">حروب</option>
-              <option value="">حياة الغرب</option>
-              <option value="">خيال</option>
-              <option value="">رسوم متحركة</option>
-              <option value="">سيرة ذاتية</option>
-              <option value="">عائلي</option>
-              <option value="">غموض</option>
-              <option value="">مدبلج بالعربي</option>
-              <option value="">نفسي</option>
+             <select  name="انواع" id="" onChange={(e)=>{setselect(e.target.value)}}>
+            <option value="مترجم">الكل</option>
+              <option value="كوميدي">كوميدي</option>
+              <option value="دراما">دراما</option>
+              <option value="رومانسي">رومانسي</option>
+              <option value="اثارة">مثير</option>
+              <option value="اكشن">اكشن</option>
+              <option value="حرب">حروب</option>
+              <option value="حياة الغرب">حياة الغرب</option>
+              <option value="خيال">خيال</option>
+              <option value="سيرة ذاتية">سيرة ذاتية</option>
+              <option value="عائلي">عائلي</option>
+              <option value="غموض">غموض</option>
+              <option value="مدبلج">مدبلج بالعربي</option>
+              <option value="نفسي">نفسي</option>
             </select>
 
             <li onClick={rangetype}>
@@ -86,7 +104,7 @@ scroll()
       </div>
       <div className='apis-en'>
     
-   { data.map((i)=>(
+   { filteredDaa.slice(0,n).map((i)=>(
       <section  key={i.id} className='a-en'>
           <div className={i.id}>
             <div className='img-film-en' onClick={()=>router.push(`./${i.id}`)}>
@@ -103,24 +121,8 @@ scroll()
           </div>
       </section>
    ))}
-   { post.map((i)=>(
-      <section  key={i.id} className='a-en'>
-          <div className={i.id}>
-            <div className='img-film-en' onClick={()=>router.push(`./${i.id}`)}>
-           
-            <Image  width={300} height={200}   src={i.Image}></Image>
-           
-            </div>
-          <p>{i.title}</p>
-          <p className='type-movie'>كوميدي 2022, دراما</p>
+</div>  <div className='more-movie' id='more-move'> <h3 onClick={loop}>شاهد المزيد</h3></div> 
 
-
-
-
-          </div>
-      </section>
-   ))}
-</div>
     </div>
   )
 }
