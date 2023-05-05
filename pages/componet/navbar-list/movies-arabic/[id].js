eb7b88f279e1affacefcc55ab7a56a44261642c0
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-05-05 19:12:24 +0300

    Add files via upload

--- pages/componet/navbar-list/movies-arabic/[id].js
+++ pages/componet/navbar-list/movies-arabic/[id].js
@@ -1,73 +1,112 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
-import data from '../../../api/movie_Arabic.json'
-import Lain4 from "../../lain4"
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import data from "../../../api/movie_Arabic.json";
+import dat from "../../../api/api_Arabic.json";
+import coments from "../../../api/comments_api.json";
 
 
+import Lain4 from "../../lain4";
 
-const Movie9 = () => {
+const Movie = () => {
+  
+  const router = useRouter();
+  const { id } = router.query;
+  const [cont, setcont] = useState(1346)
+
+  useEffect(() => {
+    localStorage.setItem("view", id);
+  }, [id]);
 
 
-  const router = useRouter()
-  const { id } = router.query
-  console.log(data[11].Link);
+  
+  var data_filter = dat.filter((element) => element.id == id);
+
   return (
-    <div className='vid'>
-    
-    <iframe src={data[id||1].Link} frameborder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen width={600} height={600} controls='true' ></iframe>
-      <div className='imgvideo' dir='rtl'>
-        <div className='likeing' >
+    <div className="vid">
+      <iframe
+        src={data[id || 1].Link}
+        frameborder="0"
+        allowFullScreen
+        controls="true"
+        width={600}
+        height={600}
+      ></iframe>
+
+      <div className="imgvideo" dir="rtl">
+        <div className="likeing">
           <section>
-          <p>25.7k</p> <Image  width={20}  height={20} src="/like.svg"   /></section>
+            <p >23</p> <Image width={20} height={20} src="/cinamana/like.svg" />
+          </section>
           <section>
-            <p>358</p>
-            <Image  width={20}  height={20} src="/like.svg"  />
-            
-           </section>
+            <p>{cont}</p>
+            <Image width={20} height={20} src="/cinamana/like.svg" />
+          </section>
+          
         </div>
-   <div>
-    <span className='descrption'>
-          <section> 
-            
-            </section>
-            <section > <div className="headmovie">
-                <h3> Transfusion </h3>
-                <h4>Sniper Redemption</h4></div>
-                <div className="taqeam">
-                 <p> IMDb</p><p>7.6</p>
-                </div>
+        <div>
+          <div >
+            {data_filter.map((el) => (
+              <div className="fullhome">
+              <div className="descrption">
+                <section >
+                  <img src={`${el.Image}`} className="descrptionimg" width={500}  />
+                </section>
+                <section>
+                  {" "}
+                  <div className="headmovie">
+                    <h3> {el.title}</h3>
+                    <h4>Sniper Redemption</h4>
+                  </div>
+                  <div className="taqeam">
+                    <p> IMDb</p>
+                    <p>7.6</p>
+                  </div>
                   <div className="alfeen">
-                  <p>جريمة</p>
-                  <p>دراما</p>
-                  <p>اثارة</p>
+                    <p>جريمة</p>
+                    <p>دراما</p>
+                    <p>اثارة</p>
                   </div>
                   <div className="time">
                     <p>1 ساعة 45 دقائق </p>
                   </div>
                   <div>
-                    <p>ينطلق عميل سابق بالقوات الخاصة في رحلة عبر عالم الجريمة من أجل إنقاذ ابنه الوحيد</p>
+                    <p>
+                      ينطلق عميل سابق بالقوات الخاصة في رحلة عبر عالم الجريمة من
+                      أجل إنقاذ ابنه الوحيد
+                    </p>
                   </div>
-                 
-               
-            </section>
-     </span>
-     {/* <ul>
-      <li>اضافة الى القائمة</li>
-      <li>اضف الى المفضلة</li>
-      <li>ابلاغ عن مشكلة</li>
-      <li>قد ترغب ايضا بمشاهدة</li>
-      </ul>*/}</div></div> 
-       <br />
-        <br />
-      <div className='lainvid' >
-        <h2>افلام قد تود مشاهدتها</h2>
-       
-         <Lain4/>
+                </section>
+              </div>
+              <div className="comments">
+                {
+                  coments.map((el)=>(
+                    <div className="comment">
+                    <section><div><img src={`/cinamana/${el.Image}`} alt=""  /> <h4>{el.Name}</h4></div><div><p>{el.date}</p></div></section>
+                    <hr />
+                    <section>
+                        <p>{el.title}</p>
+                        
+                      </section>
+                </div>
+                  ))
+                }
+                <input type="text" name="" id="" placeholder="ضع تعليقا مناسبا" />
+                
+                </div>
+                
+                </div>
+            ))}
+          </div>
+        </div>
       </div>
-     
+      <div className="lainvid">
+        <h2>افلام قد تود مشاهدتها</h2>
+        <Lain4 />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Movie9
+export default Movie;
